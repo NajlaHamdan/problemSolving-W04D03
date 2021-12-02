7c970173edd4812a8763381154a24fe4a5bea1c3
NajlaHamdan <Najla.Hamdan.elq.js@tuwaiq.edu.sa>
2021-12-02 16:59:07 +0300

    problem solving

---
+++ mostSeniorDeveloper.js
@@ -0,0 +1,8 @@
+var list = [
+    { firstName: 'Gabriel', lastName: 'X.', country: 'Monaco', continent: 'Europe', age: 49, language: 'PHP' },
+    { firstName: 'Odval', lastName: 'F.', country: 'Mongolia', continent: 'Asia', age: 38, language: 'Python' },
+    { firstName: 'Emilija', lastName: 'S.', country: 'Lithuania', continent: 'Europe', age: 19, language: 'Python' },
+    { firstName: 'Sou', lastName: 'B.', country: 'Japan', continent: 'Asia', age: 49, language: 'PHP' },
+  ];
+
+const ordered=list.sort()
